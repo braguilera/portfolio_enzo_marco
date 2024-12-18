1d6d340c2cb396ebe5066bc97c39e40e533ba9d6
braguilera <braaguileraa@gmail.com>
2024-12-17 21:12:12 -0300

    botones mejorados en sobre mi

--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { FileTreeDemo } from './magicUI/FileTreeDemo'
 import { DockDemo } from './magicUI/DockDemo'
+import { InteractiveHoverButton } from './magicUI/InteractiveHoverButton'
+import { ShimmerButton } from './magicUI/ShimmerButton'
 
 const SobreMi = () => {
+
+  const Alerta = ()  =>{
+    alert("Cv descargado")
+}
+
   return (
     <>
     <main className='flex items-start justify-center h-[100vh] w-2/6 flex-col gap-4 mx-auto my-0 bg-slate-400'>
@@ -17,10 +24,18 @@ const SobreMi = () => {
       <section className='flex flex-col gap-1'>
         <h2 className='flex text-slate-900 text-2xl font-semibold'>Enzo Marco</h2>
         <h3 className='text-muted-foreground font-light'>Ingeniero de software</h3>
+        <p>Soy un estudiante universitario de tercer año con el objetivo de adentrarme dentro del mundo laboral, ocupando puestos relacionados con el back-end que me ofrecan la posibilidad de crecer dentro de este ámbito</p>
         <p>texto largo</p>
-        <p>texto largo</p>
-        <button> Descargar CV </button>
-        <button> Contratame </button>
+
+        <footer className='flex w-3/4 h-full self-center gap-2'>
+          <ShimmerButton className="shadow-2xl w-full" funcion={Alerta}>
+              <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+              Descargar CV
+              </span>
+          </ShimmerButton>
+
+          <InteractiveHoverButton></InteractiveHoverButton>
+        </footer>
       </section>
       
       <section>
